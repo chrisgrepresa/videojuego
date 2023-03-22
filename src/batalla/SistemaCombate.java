97fa8554c19bd7741aa4aa34d00b90962a916730
chrisgrepresa <chrisgrepresacomics@gmail.com>
2023-03-22 17:52:13 +0100

    Nueva rama para probar otra manera de hacer bucle while

--- src/batalla/SistemaCombate.java
+++ src/batalla/SistemaCombate.java
@@ -11,50 +11,46 @@ public class SistemaCombate {
 
     List<Personaje> personaje = new ArrayList<>();
     List<Enemigo> enemigo = new ArrayList<>();
+
+
     Random random = new Random();
 
 
     public void atacar(Personaje personaje, Enemigo enemigo) {
 
-        boolean faltanAtaques = true;
+        boolean combateTerminado = true;
         boolean personajeAtaca = false;
         boolean enemigoAtaca = false;
 
 
-        while (faltanAtaques == true) {
-            if (personajeAtaca == false) {
-                int damageOne = random.nextInt(1, 10);
-                int resultadoUno = (personaje.getAtaque() + damageOne) - (enemigo.getDefensa() - damageOne);
-                if (resultadoUno > 0) {
-                    enemigo.setSalud(enemigo.getSalud() - resultadoUno);
-                }
-                personajeAtaca = true;
+        while (!combateTerminado) {
+            int damageOne = random.nextInt(1, 10);
+            int resultadoUno = (personaje.getAtaque() + damageOne) - (enemigo.getDefensa() - damageOne);
+            if (resultadoUno > 0) {
+                enemigo.setSalud(enemigo.getSalud() - resultadoUno);
             }
-
-            if (enemigoAtaca == false) {
+            if (enemigo.getSalud() > 0) {
                 int damageTwo = random.nextInt(1, 10);
                 int resultadoDos = (enemigo.getAtaque() + damageTwo) - (personaje.getDefensa() - damageTwo);
                 if (resultadoDos > 0) {
                     personaje.setSalud(personaje.getSalud() - resultadoDos);
 
                 }
-                enemigoAtaca = true;
             }
-
-            if (personajeAtaca == true && enemigoAtaca == true) {
-                faltanAtaques = false;
-            } //creo que aquí no hace falta else
+            if (personaje.getSalud() <= 0 && enemigo.getSalud() <= 0) {
+                combateTerminado = true;
+            }
         }
         System.out.println("Se acabó el combate");
-    }
 
-    public void huir(Personaje personaje){
-        int diceScape = random.nextInt(1, 10);
-        if(diceScape>=8){
-            System.out.println("Escapas del combate");
-        }
-        else {
-            System.out.println("¡Que comience el combate!");
+
+        public void huir(Personaje personaje){
+            int diceScape = random.nextInt(1, 10);
+            if (diceScape >= 8) {
+                System.out.println("Escapas del combate");
+            } else {
+                System.out.println("¡Que comience el combate!");
+            }
         }
     }
 }
